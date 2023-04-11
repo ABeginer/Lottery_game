037252e61c08082ef821bea14ccb0a410ca8a7b6
Thuận <105507543+ABeginer@users.noreply.github.com>
2023-04-11 20:47:38 +0700

    delete

--- Vehicles/Motorbike.java
+++
@@ -1,9 +0,0 @@
-package Vehicles;
-
-public class Motorbike extends Vehicles {
-    private final int  SIZE = 1;
-    public Motorbike(String BRAND_NAME, int speed, String color) {
-        super(BRAND_NAME, speed, color);
-    }
-    
-}
